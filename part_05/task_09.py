6d20c31b6ff5c695b68a2ae71d70227793c01af1
Eugene Elantsev <eugene_el@mail.ru>
2022-05-19 09:55:49 +0500

    task 09 done

--- part_05/task_09.py
+++ part_05/task_09.py
@@ -2,8 +2,8 @@
 # приоритет операций стандартный. Пример: 2+2 => 4; 1+2*3 => 7; 1-2*3 => -5;
 # Добавить возможность использования скобок, меняющих приоритет операций. Пример: 1+2*3 => 7; (1+2)*3 => 9
 
-expression_str = '-1+6/3*2+(1+2)*3'
-
+expression_str = '-1+6/(3*2)+(1+2)*3'
+print(f'Выражение: {expression_str}')
 
 def math_expr(init_str):
     # Производим действия умножения и деления, выражение в строке заменяем на результат действия
@@ -13,12 +13,12 @@ def math_expr(init_str):
         init_str = init_str[:(i-1)] + temp + init_str[(i+2):]
     while '/' in init_str:
         i = init_str.index('/')
-        temp = str(int(init_str[i-1]) / int(init_str[i+1]))
+        temp = str(int(init_str[i-1]) // int(init_str[i+1]))
         init_str = init_str[:(i-1)] + temp + init_str[(i+2):]
     # В строке добавляем "+" перед отрицательными числами
     init_str = ''.join([i if i != '-' else '+-' for i in init_str])
     # Разбиваем строку (разделитель +), возвращаем сумму
-    return sum([float(i) for i in init_str.split('+') if i != ''])
+    return sum([int(i) for i in init_str.split('+') if i != ''])
 
 
 # Определяем наличие скобок в выражении.
@@ -31,9 +31,8 @@ while '(' in expression_str:
     temp = math_expr(temp_expression)
     expression_str = expression_str[:(left_bracket)] + str(temp) + expression_str[(right_bracket+1):]
 
-print(f'Выражение: {expression_str}')
 print(f'Результат вычисления: {math_expr(expression_str)}')
 
 # Output:
-# Выражение: -1+6/3*2+3.0*3
-# Результат вычисления: 3.0
+# Выражение: -1+6/(3*2)+(1+2)*3
+# Результат вычисления: 9
