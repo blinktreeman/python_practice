8d9b1d914eb80fe14dfeab243813e972e75658c7
Eugene Elantsev <eugene_el@mail.ru>
2022-06-01 12:04:54 +0500

    add wiki search

--- part_07/main.py
+++ part_07/main.py
@@ -1,12 +1,16 @@
 import settings
 import telebot
-import random
+import requests
+import json
+import wikipedia
 
 bot = telebot.TeleBot(settings.BOT_TOKEN)
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    mess = '<u>Для работы с калькулятором введите /calc "выражение"</u>'
+    mess = 'Для работы с калькулятором введите <u>/calc "выражение"</u>\n'
+    mess += 'Получить рандомную утку <u>/duck</u>\n'
+    mess += 'Поиск по wikipedia <u>/wiki "что ищем"</u>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 @bot.message_handler(commands=['calc'])
@@ -19,20 +23,25 @@ def calc(message):
     mess = result
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
-@bot.message_handler(commands=['candy'])
-def candy(message):
-    candies = random.randint(20, 30)
-    #print(candies)
-    while candies > 0:
-        mess = f'Конфет на столе {candies}\n'
-        mess += 'Можно взять до 4 шт\n'
-        bot.send_message(message.chat.id, mess, parse_mode='html')
+@bot.message_handler(commands=['duck'])
+def duck(message):
+    url = 'https://random-d.uk/api/random'
+    response = requests.get(url)
+    duck_link = json.loads(response.text)
+    mess = duck_link['url']
+    bot.send_message(message.chat.id, mess, parse_mode='html')
 
-        @bot.message_handler()
-        def input_candy(message):
-            input()
-            get_candies = int(message.text)
-            print(get_candies)
-    #candies
+@bot.message_handler(commands=['wiki'])
+def wiki(message):
+    args = message.text.split()
+    arg = ''
+    if len(args) > 1:
+        arg = ' '.join(args[1:])
+    response = wikipedia.search(arg, results=10, suggestion=True)
+    mess = ''
+    mess += f'Результаты поиска для <b>{arg}</b>\n'
+    for i in response[0]:
+        mess += f'{i}\n'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
 
 bot.polling(none_stop=True)
